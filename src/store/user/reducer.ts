7029dd72d2b54a7a9f9947ee3c0abcf080e8bb0b
Sjouke91 <s.bosma1991@gmail.com>
2020-11-22 23:45:27 +0100

    Added user store

---
+++ src/store/user/reducer.ts
@@ -0,0 +1,27 @@
+import { LOG_OUT, LOGIN_SUCCESS, TOKEN_STILL_VALID } from "./actions";
+import { UserActionTypes } from "./types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  name: null,
+  email: null,
+  id: null,
+};
+
+export default (state = initialState, action: UserActionTypes) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return { ...state, ...action.payload };
+
+    case LOG_OUT:
+      localStorage.removeItem("token");
+      return { ...initialState, token: null };
+
+    case TOKEN_STILL_VALID:
+      return { ...state, ...action.payload };
+
+    default:
+      return state;
+  }
+};
---
+++ src/store/user/types.ts
@@ -0,0 +1,43 @@
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const TOKEN_STILL_VALID = "TOKEN_STILL_VALID";
+export const LOG_OUT = "LOG_OUT";
+
+export interface UserWithToken {
+  name: string;
+  email: string;
+  token: string;
+  id: number;
+}
+
+export interface UserWithoutToken {
+  name: string;
+  email: string;
+  id: number;
+}
+
+interface LoginSuccesAction {
+  type: typeof LOGIN_SUCCESS;
+  payload: UserWithToken;
+}
+
+interface tokenStillValidAction {
+  type: typeof TOKEN_STILL_VALID;
+  payload: UserWithoutToken;
+}
+
+interface logOutAction {
+  type: typeof LOG_OUT;
+}
+
+export type UserActionTypes =
+  | LoginSuccesAction
+  | tokenStillValidAction
+  | logOutAction;
+
+export interface UserState {
+  user: {
+    token: string;
+    name: string;
+    email: string;
+  };
+}
