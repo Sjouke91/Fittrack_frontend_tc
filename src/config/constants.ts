872e0472d06ad970fc1f911dd819f9b6181f1ae9
Sjouke91 <s.bosma1991@gmail.com>
2020-12-03 12:36:00 +0100

    config

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,2 @@
-export const apiUrl = "https://fittrack-be.herokuapp.com";
-
+export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4001";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
